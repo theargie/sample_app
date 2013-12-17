22de83c243e26bea62cccbabaf8075c23164247a
Santi Botta <santibotta@hotmail.com>
2013-12-17 18:41:08 -0300

    initial commit

---
+++ config/initializers/secret_toke(backup).rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0c413a5e6adbc9c83bcc16a451d3d4c3f765142e0e0cdaa41d3e5035e306ca6eea1d0d3776e7ff663fb73b5323c880e3d1821e6a0bc441730306722c6b2f9072'
